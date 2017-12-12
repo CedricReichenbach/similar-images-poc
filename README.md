57f8037789642aab3614c45a7accdc7b4266a485
Cedric Reichenbach <cedric.reichenbach@magnolia-cms.com>
2017-12-12 10:45:09 +0100

    Initial project setup; some test images

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # similar-images-poc
+
+Simple POC project to train the top layer of a pre-trained network on a handful of selected images.
---
+++ src/main/java/info/magnolia/ai/similarity/App.java
@@ -0,0 +1,8 @@
+package info.magnolia.ai.similarity;
+
+public class App {
+
+    public static void main(String[] args) {
+        // TODO
+    }
+}
